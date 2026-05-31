d2aa082d65f16ce21ca156a6305b498706a7eced
riya badoini <riya.badoni@logicladder.com>
2026-05-31 14:51:26 +0530

    setup

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+require("dotenv").config();
+
+const app = express();
+
+app.use(express.json());
+
+const userRoutes = require("./routes/userRoutes");
+app.use("/api/users", userRoutes);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
